2a9aef67021572153cfaeb1932d428cbf35c1638
demonterminal <demonterminal@gmail.com>
2024-07-17 12:47:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener('keypress', function (event) {
   var cin = document.getElementById('path').value;
   if (event.key === 'Enter'){
-    document.body.innerHTML += '<a href="'+cin+'" id="a"></a>';
+    document.body.innerHTML += '<a href="/repository/'+cin+'" id="a"></a>';
     document.getElementById('a').click();
   }
 })
